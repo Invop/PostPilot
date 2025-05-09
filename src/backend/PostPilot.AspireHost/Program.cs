35574d3fe85310c491bd1d7b2a8c706c22c401f1
Andrii Shandyk <shandrykjj512@gmail.com>
2025-05-09 17:57:09 +0300

    Add Next.js frontend and integrate with aspire

--- src/backend/PostPilot.AspireHost/Program.cs
+++ src/backend/PostPilot.AspireHost/Program.cs
@@ -1,3 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddNpmApp("client", "../../frontend/postpilot","dev")
+  .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
+  .WithHttpEndpoint(env: "PORT")
+  .WithExternalHttpEndpoints();
 builder.Build().Run();
