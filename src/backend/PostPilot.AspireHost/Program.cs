bfa398431fd4df650c9eaaa65aec801410762dda
Andrii Shandyk <shandrykjj512@gmail.com>
2025-05-11 16:00:23 +0300

    The initial frontend application was temporarily deleted until a clear structure is built
    Related concomitant integration in Aspire Host are also removed

--- src/backend/PostPilot.AspireHost/Program.cs
+++ src/backend/PostPilot.AspireHost/Program.cs
@@ -1,7 +1,4 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddNpmApp("client", "../../frontend/postpilot","dev")
-  .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
-  .WithHttpEndpoint(env: "PORT")
-  .WithExternalHttpEndpoints();
+
 builder.Build().Run();
